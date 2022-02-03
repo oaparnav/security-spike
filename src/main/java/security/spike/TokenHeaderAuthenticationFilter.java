5a7097d989af1a932135b86daec3de3734cb80ff
Aparna venkata <oaparnav@MGC12FT247MD6N.chennaigtbc.ford.com>
2022-02-03 18:00:53 +0530

    Commited local changes

--- src/main/java/security/spike/TokenHeaderAuthenticationFilter.java
+++ src/main/java/security/spike/TokenHeaderAuthenticationFilter.java
@@ -4,16 +4,12 @@ import java.io.IOException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.web.filter.GenericFilterBean;
 
 public class TokenHeaderAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
 
